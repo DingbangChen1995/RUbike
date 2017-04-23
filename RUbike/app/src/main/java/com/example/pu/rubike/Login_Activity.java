d8ab8203b69415c72c2d7bad2a9de685f2b4d34a
PuChen7 <puchen8295@gmail.com>
2017-04-22 20:13:50 -0400

    updated login and register layout

--- RUbike/app/src/main/java/com/example/pu/rubike/Login_Activity.java
+++ RUbike/app/src/main/java/com/example/pu/rubike/Login_Activity.java
@@ -1,13 +1,42 @@
 package com.example.pu.rubike;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class Login_Activity extends AppCompatActivity {
+public class Login_Activity extends AppCompatActivity implements View.OnClickListener{
+
+    Button bLogin;
+    EditText etUsername, etPassword;
+    TextView linkRegisterHere;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_);
+
+        etUsername = (EditText) findViewById(R.id.etUsername);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        bLogin = (Button) findViewById(R.id.bLogin);
+        linkRegisterHere = (TextView) findViewById(R.id.linkRegisterHere);
+
+        bLogin.setOnClickListener(this);
+        linkRegisterHere.setOnClickListener(this);
+    }
+
+    public void onClick(View v){
+        switch(v.getId()){
+            case R.id.bLogin:
+
+                break;
+
+            case R.id.linkRegisterHere:
+                startActivity(new Intent(this, Register_Activity.class));
+                break;
+        }
     }
 }
--- RUbike/app/src/main/java/com/example/pu/rubike/Register_Activity.java
+++ RUbike/app/src/main/java/com/example/pu/rubike/Register_Activity.java
@@ -2,12 +2,33 @@ package com.example.pu.rubike;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class Register_Activity extends AppCompatActivity {
+public class Register_Activity extends AppCompatActivity implements View.OnClickListener{
+
+    Button bRegister;
+    EditText etName, etPassword, etRUID, etEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_);
+
+        etName = (EditText) findViewById(R.id.etName);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        etRUID = (EditText) findViewById(R.id.etRUID);
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        bRegister = (Button) findViewById(R.id.bRegister);
+
+        bRegister.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.bRegister:
+        }
     }
 }
